135affd382e1ed2cd8a1c2f6fc96bdf44e1ffc18
VirreV <61963500+VirreV@users.noreply.github.com>
2020-10-12 14:48:09 +0200

    Minor fixes
    Fixed blur on searchAlts aswell as som coloring, next up making searches work

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,18 @@
 let dropListDiv = document.querySelector("#searchAlts");
-function AddDropDiv(str) {
-    let d = document.createElement("div")
-    d.innerHTML = str;
+function AddDropDiv(roomNr, nameStr) {
+    let d = document.createElement("div");
     d.className = "searchAltsDiv";
+    let room = document.createElement("div");
+    room.innerHTML = roomNr;
+    let name = document.createElement("div");
+    name.innerHTML = nameStr;
+    d.appendChild(room);
+    d.appendChild(name);
     dropListDiv.appendChild(d);
-    console.log(str);
+    console.log(roomNr, nameStr);
+    let refDiv = document.querySelector("#searchAlts");
+    let overlapDiv = document.querySelector("#overlap");
+    overlapDiv.style.height = refDiv.style.height;
 }
 
 let SubmitFunc = function (event) {
@@ -17,11 +25,14 @@ roomSearch.addEventListener("submit", SubmitFunc, true);
 
 document.querySelector("#searchAlts").addEventListener('scroll', function(event)
 {
-    let element = event.target;
-    if (element.scrollHeight - element.scrollTop - element.clientHeight < 30) {
-        element.classList.add("scrolled");
-    } else element.classList.remove("scrolled");
+    CheckScroll(event.target);
 });
+
+function CheckScroll(element){
+    if (element.scrollHeight - element.scrollTop - element.clientHeight < 30) {
+        document.querySelector("#overlap").classList.add("scrolled");
+    }else document.querySelector("#overlap").classList.remove("scrolled");
+}
 
 fetch('DATA/data.json').then(response => response.json()).then(data => {
     document.querySelector("#txt-search").addEventListener("keyup", () => {
@@ -30,8 +41,8 @@ fetch('DATA/data.json').then(response => response.json()).then(data => {
         let count = 0;
         data.forEach(element => {
             element.forEach(ele => {
-                if (ele.room.substring(0, str.length).toLowerCase() == str.toLowerCase() && str.length > 0) {
-                    AddDropDiv(ele.room);
+                if ((ele.room.substring(0, str.length).toLowerCase() == str.toLowerCase() || ele.name.substring(0, str.length).toLowerCase() == str.toLowerCase()) && str.length > 0) {
+                    AddDropDiv(ele.room, ele.name);
                     count++;
                 }
             });
@@ -41,5 +52,9 @@ fetch('DATA/data.json').then(response => response.json()).then(data => {
         } else if (!document.querySelector(".searchBox").classList.contains("dropActive")) {
             document.querySelector(".searchBox").classList.add("dropActive");
         }
+        console.log(document.querySelector("#searchAlts").childElementCount);
+        if(document.querySelector("#searchAlts").childElementCount < 7){
+            document.querySelector("#overlap").classList.add("scrolled");
+        } else CheckScroll(document.querySelector("#searchAlts"));
     })
 });
